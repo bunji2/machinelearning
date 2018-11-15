e9bcb800068248ed1e8c3e4e8a1c2d391afe44b5
Bunji Square <bunji2@users.noreply.github.com>
2018-11-15 17:56:06 +0900

    update

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/sjwhitworth/golearn/base"
 	"github.com/sjwhitworth/golearn/evaluation"
 	"github.com/sjwhitworth/golearn/knn"
+	"github.com/sjwhitworth/golearn/neural"
 )
 
 const (
@@ -27,7 +28,7 @@ func main() {
 func run() int {
 
 	if len(os.Args) < 3 {
-		fmt.Fprintf(os.Stderr, "Usage: %s [knn | nn] [ iris | mnist ]", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s [knn | nn | nn2] [ iris | mnist ]", os.Args[0])
 		return 1
 	}
 
@@ -43,6 +44,10 @@ func run() int {
 		err = nnIRIS()
 	} else if cmd == "nn" && data == "mnist" {
 		err = nnMNIST()
+	} else if cmd == "nn2" && data == "iris" {
+		err = nn2IRIS()
+	} else if cmd == "nn2" && data == "mnist" {
+		err = nn2MNIST()
 	} else {
 		err = fmt.Errorf("unknown args cmd = %s data = %s", cmd, data)
 	}
@@ -91,6 +96,24 @@ func nnIRIS() (err error) {
 	return
 }
 
+// nnIRIS : NN を用いた評価
+func nn2IRIS() (err error) {
+	var rawData base.FixedDataGrid
+
+	// IRISデータの取得
+	rawData, err = base.ParseCSVToInstances(irisDataFile, dataHeader)
+
+	if err != nil {
+		return
+	}
+
+	// データセットを学習用とテスト様に分離
+	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.50)
+
+	err = evalNN2(trainData, testData)
+	return
+}
+
 func knnMNIST() (err error) {
 	var rawData base.FixedDataGrid
 	t1 := time.Now()
@@ -118,7 +141,7 @@ func knnMNIST() (err error) {
 func nnMNIST() (err error) {
 	var rawData base.FixedDataGrid
 	t1 := time.Now()
-	rawData, err = mnistdata.Load(mnistImageFile, mnistLabelFile, 1000)
+	rawData, err = mnistdata.Load(mnistImageFile, mnistLabelFile, 100000)
 	t2 := time.Now()
 	fmt.Println("elapse time of loading :", t2.Sub(t1))
 	if err != nil {
@@ -139,12 +162,36 @@ func nnMNIST() (err error) {
 	return
 }
 
+func nn2MNIST() (err error) {
+	var rawData base.FixedDataGrid
+	t1 := time.Now()
+	rawData, err = mnistdata.Load(mnistImageFile, mnistLabelFile, 1000)
+	t2 := time.Now()
+	fmt.Println("elapse time of loading :", t2.Sub(t1))
+	if err != nil {
+		return
+	}
+
+	// データセットを学習用とテスト用に分離
+	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.1)
+	t3 := time.Now()
+	fmt.Println("elapse time of Spliting :", t3.Sub(t2))
+
+	cols, rows := trainData.Size()
+	fmt.Printf("trainData = %d x %d\n", rows, cols)
+	cols, rows = testData.Size()
+	fmt.Printf("testData = %d x %d\n", rows, cols)
+
+	err = evalNN2(trainData, testData)
+	return
+}
+
 // evalNN : NN による分析
 func evalNN(trainData, testData base.FixedDataGrid, resolveClassName func([]float64) string, convData func(base.FixedDataGrid) [][][]float64) (err error) {
 	// 分類器の初期化
 	// hiddenSize, epochs, lRate, mFactor, debug, resolveClassName, convData
 	//cls := nn.NewClassifier(100, 10000, 0.4, 0.6, false, irisdata.ResolveClassName, irisdata.ConvData)
-	cls := nn.NewClassifier(100, 5000, 0.4, 0.6, true, resolveClassName, convData)
+	cls := nn.NewClassifier(100, 10000, 0.4, 0.6, true, resolveClassName, convData)
 
 	// 学習
 	t1 := time.Now()
@@ -194,6 +241,35 @@ func evalKNN(trainData, testData base.FixedDataGrid) (err error) {
 	if err != nil {
 		return
 	}
+
+	//fmt.Println(predictions)
+
+	// スコア算出
+	var confusionMat map[string]map[string]int
+	confusionMat, err = evaluation.GetConfusionMatrix(testData, predictions)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("kNN (golearn)")
+	fmt.Println(evaluation.GetSummary(confusionMat))
+	return
+}
+
+// evalNN2 : NN による分析
+func evalNN2(trainData, testData base.FixedDataGrid) (err error) {
+	cols, rows := trainData.Size()
+	fmt.Printf("trainData = %d x %d\n", rows, cols)
+
+	// MLNを初期化
+	cls := neural.NewMultiLayerNet([]int{cols - 1, 100, 1})
+
+	// 学習
+	cls.Fit(trainData)
+
+	// 予測
+	var predictions base.FixedDataGrid
+	predictions = cls.Predict(testData)
 
 	//fmt.Println(predictions)
 
